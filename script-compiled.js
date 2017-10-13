b2e97744056f6238ea04b88a503e163a82e3c8a2
makaron3d <makaron3d@gmail.com>
2017-10-13 11:52:46 +0200

    0006

--- script-compiled.js
+++ script-compiled.js
@@ -1,10 +1,61 @@
 'use strict';
 
-var _console;
+//----1---
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var one = 'Hello';
+var two = 'world!';
+console.log(one + ' ' + two);
 
-var greeting = 'Hello';
-var name = ' World!';
+//console.log(...greeting, ...name);
 
-(_console = console).log.apply(_console, _toConsumableArray(greeting).concat(_toConsumableArray(name)));
+//-------------2------------------
+
+//const a = prompt('podaj dyfrę "a":' + '');
+//const b = prompt('podaj cyfrę "b":' + '');
+
+var multiply = function multiply() {
+    var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 4;
+    var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+    return a * b;
+};
+console.log(multiply(8, 9));
+
+//--------3-----------
+
+//setTimeout(() => console.log('heja, heja'), 3000);
+
+function averages() {
+    //console.log(toAdd);
+    var result = 0;
+
+    for (var _len = arguments.length, toAdd = Array(_len), _key = 0; _key < _len; _key++) {
+        toAdd[_key] = arguments[_key];
+    }
+
+    for (var i = 0; i < toAdd.length; i++) {
+        result += toAdd[i];
+    }
+    return result / toAdd.length;
+};
+
+console.log(averages(2, 6, 8, 4));
+
+//------------------4---------------
+
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+var result = 0;
+grades.forEach(function (grade) {
+    result += grade;
+});
+
+console.log(result / grades.length);
+
+//----------5------------
+
+var array = [1, 4, 'Iwona', false, 'Nowak'];
+var first = array[0],
+    last = array[2];
+
+console.log(first, last);
--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-const greeting = 'Hello';
-const name = ' World!';
+//----1---
 
-console.log(...greeting, ...name);
+const one = 'Hello';
+const two = 'world!';
+console.log(`${one} ${two}`);
+
+//console.log(...greeting, ...name);
+
+//-------------2------------------
+
+//const a = prompt('podaj dyfrę "a":' + '');
+//const b = prompt('podaj cyfrę "b":' + '');
+
+let multiply = (a=4,b=3) => a * b;
+console.log(multiply(8,9));
+
+
+//--------3-----------
+
+//setTimeout(() => console.log('heja, heja'), 3000);
+
+function averages(...toAdd) {
+    //console.log(toAdd);
+    let result = 0;
+    for (let i = 0; i < toAdd.length; i++) {
+        result += toAdd[i];
+    } 
+    return result/toAdd.length;
+};
+
+console.log(averages(2,6,8,4));
+
+//------------------4---------------
+
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+var result = 0;
+grades.forEach(function(grade) {
+  result += grade;
+});
+
+console.log(result/grades.length);
+
+
+//----------5------------
+
+const array = [1, 4,'Iwona', false, 'Nowak'];
+const [first, ,last] = array;
+console.log(first, last);
+
+
+
